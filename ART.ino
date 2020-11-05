dd973723fad4a460ebe1961e6e5a29f7be1aab07
Elijah Hewer <phenomhewer@gmail.com>
2020-11-05 01:24:51 -0800

    completed basic timer functionality

--- ART.ino
+++ ART.ino
@@ -1,5 +1,12 @@
+/*
+ * by Elijah Hewer
+ * 
+ * November 2020
+ */
+
 #include <LiquidCrystal.h>
 
+// FSM states
 #define STATE_MENU 0
 #define STATE_COUNT 1
 #define STATE_PAUSE 2
@@ -31,13 +38,11 @@ void setup() {
   pinMode(butRPin, INPUT_PULLUP);
 }
 
+// timing variables
+int sec;
+unsigned long msLast;
+
 void loop() {
-  /*
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  int curTime = millis() / 1000;
-  lcd.print(curTime);
-  */
   /*
   if (!buz_state && curTime % 5 == 0) {
     tone(buzPin, 1000);
@@ -47,16 +52,77 @@ void loop() {
     buz_state = false;
   }
   */
-
+  
   // read from inputs
   int butG = digitalRead(butGPin);
   int butR = digitalRead(butRPin);
-  int pot = readPot()
+  int pot = readPot();
   
   // FSM
   switch (state) {
-    case STATE_MENU:   
+    case STATE_MENU:
+      lcd.setCursor(0, 0);
+      lcd.print("Round time:");
+      
       switch (pot) {
+        case 0:
+          sec = 30;
+          break;
+        case 1:
+          sec = 60;
+          break;
+        case 2:
+          sec = 90;
+          break;
+        case 3:
+          sec = 120;
+          break;
+        case 4:
+          sec = 150;
+          break;
+        case 5:
+          sec = 180;
+          break;
+        case 6:
+          sec = 300;
+          break;
+        default:
+          sec = 0;
+          break;
+      }
+
+      displayTime(sec);
+
+      // continue condition
+      if (butG == HIGH) {
+        clearLine(0);
+        state = STATE_COUNT;
+        msLast = millis();
+      }
+      
+      break;
+      
+    case STATE_COUNT:
+      lcd.setCursor(0, 0);
+      lcd.print("GO!!");
+      
+      updateTime();
+      displayTime(sec);
+
+      // continue condition
+      if (sec <= 0) {
+        state = STATE_MENU;
+      }
+      
+      break;
+      
+    case STATE_PAUSE:
+      break;
+      
+    default:
+      switch (pot) {
+        case 0:
+          break;
         case 1:
           break;
         case 2:
@@ -65,26 +131,24 @@ void loop() {
           break;
         case 4:
           break;
-        case 5
+        case 5:
+          break;
+        case 6:
           break;
         default:
           break;
       }
       break;
-    case STATE_COUNT:
-      break;
-    case STATE_PAUSE:
-      break;
-    default:
-      break;
   }
 }
 
-// returns a value [1, 5] from the potentiometer
+// returns a value [0, 6] from the potentiometer
 int readPot() {
   int val = analogRead(potPin);
-  
-  if (val >= 800) {
+
+  if (val >= 920) {
+    return 6;
+  } else if (val >= 800) {
     return 5;
   } else if (val >= 600) {
     return 4;
@@ -92,8 +156,30 @@ int readPot() {
     return 3;
   } else if (val >= 200){
     return 2;
-  } else {
+  } else if (val >= 80){
     return 1;
+  } else {
+    return 0;
+  }
+}
+
+// takes time in seconds and displays it in M:SS format
+void displayTime(int sec) {
+  char buff[4];
+  sprintf(buff, "%i:%02i", sec / 60, sec % 60);
+  
+  lcd.setCursor(0,1);
+  lcd.print(buff);
+}
+
+// updates global variables to represent changes in seconds
+// unsigned long to handle millis() rollover
+void updateTime() {
+  unsigned long msCur = millis();
+  
+  if (msCur - msLast > 1000) {
+    msLast = msCur;
+    sec--;
   }
 }
 
